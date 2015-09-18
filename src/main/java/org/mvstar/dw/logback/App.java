46f6aeaa15343774bb9712642853800e96e3e4c5
mv200580 <mv200580@gmail.com>
2015-09-18 20:04:47 +0500

    Initial commit

---
+++ src/main/java/org/mvstar/dw/logback/App.java
@@ -0,0 +1,13 @@
+package org.mvstar.dw.logback;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
